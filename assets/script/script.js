0fea9cec45209443919ceec7fb873578c551b734
Samuli Jarvinen <frye@github.com>
2022-01-04 12:08:52 -0800

    Adding contact form placeholder

---
+++ assets/script/script.js
@@ -0,0 +1,4 @@
+var pressedButton = document.getElementsByName("submitButton")[0];
+pressedButton.addEventListener("click", function (event) {
+   alert("Submit function has not been implemented yet.")
+})
