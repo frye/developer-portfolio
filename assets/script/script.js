e3309abd7233cc42bb1fd387410918313d5ffc6a
Samuli Jarvinen <frye@github.com>
2022-02-13 22:08:02 -0800

    Updated displayed projects and project card layout. Removed unnecessary files.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,5 @@
-var pressedButton = document.getElementsByName("submitButton")[0];
-pressedButton.addEventListener("click", function (event) {
-   alert("Submit function has not been implemented yet.")
-})
+// Removed the contact form for now and replaced with the e-mail link.
+// var pressedButton = document.getElementsByName("submitButton")[0];
+// pressedButton.addEventListener("click", function (event) {
+//    alert("Submit function has not been implemented yet.")
+// })
